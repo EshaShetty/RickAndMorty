cb2f60857bc62bdd10a31fde1ed20c08860d22a2
EshaShetty <esha.dshetty@gmail.com>
2022-08-24 13:41:26 -0400

    Added Character Fragment and a blank test fragment

---
+++ app/src/main/java/com/example/rickandmorty/CharacterFragment.kt
@@ -0,0 +1,11 @@
+package com.example.rickandmorty
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CharacterFragment : Fragment() {
+
+}
